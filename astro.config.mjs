c49aa2ba0e849cbdcda2ac86ade472bd8c28e473
jacobi <jake.grozdanovski.13@gmail.com>
2026-01-28 18:59:11 +1100

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 // - set `site` to your eventual Pages URL
 // - set `base` to your repo name (or "" if using a custom domain/root)
 export default defineConfig({
+  site: "https://groz-dev.github.io",
+  base: "/jv-kitchen",
   integrations: [tailwind()],
 });
 
